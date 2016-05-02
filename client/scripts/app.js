4bf68dce562e69cc3f02167db28820111eeafc46
nismodao <nam.d.dao@gmail.com>
2016-05-02 15:05:34 -0700

    completes init, send, fetch, clearmessages, addmessages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,48 @@
 // YOUR CODE HERE:
 
+var app = {};
+app.server = 'https://api.parse.com/1/classes/messages';
+
+app.init = function() {};
+
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Data received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get messages', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $("#chats").html("");
+};
+
+app.addMessage = function(message) {
+  $('#chats').append('<span><\span>').html(message.text);
+};
