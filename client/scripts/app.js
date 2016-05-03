3ba852047c8fb07908d9fee113fb09d08458ae03
nismodao <nam.d.dao@gmail.com>
2016-05-02 17:32:49 -0700

    added input form, successful message send

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,32 @@
 // YOUR CODE HERE:
 
 var app = {};
+var counter = 0;
+
+
 app.server = 'https://api.parse.com/1/classes/messages';
 
-app.init = function() {};
+app.init = function() {
+  $(".username").click(function () { 
+    var userName = $(this).html();
+    console.log(app.addFriend);
+    app.addFriend(userName);
+  });
+
+  $('#send .submit').submit(function(event) {
+    event.preventDefault();
+    console.log('submittttting');
+    counter++;
+    var message = $('#message').val();
+    app.handleSubmit(message);
+  });  
+
+
+};
 
 app.send = function(message) {
+
+  console.log('attempt to send');
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/messages',
@@ -44,5 +65,37 @@ app.clearMessages = function() {
 };
 
 app.addMessage = function(message) {
-  $('#chats').append('<span><\span>').html(message.text);
+  var text = message.text;
+  var user = message.username;
+  var room = message.roomname;
+
+  var $chat = $('<div class=message>' + 
+      '<span class=username>' + user + '</span>' + ':\n' + 
+      '<p class=msg>' + text + '</p>' + 
+    '</div>');
+  $chat.appendTo('#chats');
+  // $('#chats').append('<span></span>');
 };
+
+app.addRoom = function(roomName) {
+  $('#roomSelect').append('<span></span>');
+};
+
+app.friendsList = {};
+
+app.addFriend = function (friendName) {
+  app.friendsList[friendName] = true;
+  $('#main').append('<div class = username>'+ friendName + '</div>');
+  //$('#main > .username').html(friendName); /// class = username
+};
+
+app.handleSubmit = function( message ) {
+
+  app.send(message);
+};
+
+app.init();
+
+
+
+
